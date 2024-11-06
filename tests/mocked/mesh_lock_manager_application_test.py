96fa0d2e692e8aa54013e0996cb61bceac82415b
james-bradley-nhs <james.bradley7@nhs.net>
2024-11-06 16:07:27 +0000

    em-933: fix lock manager unit test

--- tests/mocked/mesh_lock_manager_application_test.py
+++ tests/mocked/mesh_lock_manager_application_test.py
@@ -2,7 +2,7 @@ from unittest.mock import patch
 
 from mesh_lock_manager_application import MeshLockManagerApplication
 
-from tests.mocked.mesh_testing_common import was_value_logged
+from mocked.mesh_testing_common import was_value_logged
 
 app = MeshLockManagerApplication()
 
@@ -10,20 +10,23 @@ app = MeshLockManagerApplication()
 @patch("shared.application.release_lock")
 def test_happy_path(rel_lock, capsys):
     mock_event = {
-        "EventDetail": {"lock_name": "test_lock", "execution_id": "test_execution"},
-        "Operation": "remove",
+        "EventDetail": {
+            "body": {"lock_name": "test_lock", "execution_id": "test_execution"}
+        },
+        "Operation": "release",
     }
-    mock_context = {}
 
-    app.main(mock_event, mock_context)
+    app.main(mock_event, {})
     logs = capsys.readouterr()
     assert was_value_logged(logs.out, "MESHLOCK0002", "Log_Level", "INFO")
 
 
 def test_operation_not_remove(capsys):
     mock_event = {
-        "EventDetail": {"lock_name": "test_lock", "execution_id": "test_execution"},
-        "Operation": {"not remove"},
+        "EventDetail": {
+            "body": {"lock_name": "test_lock", "execution_id": "test_execution"}
+        },
+        "Operation": {"not release"},
     }
 
     app.main(mock_event, {})
